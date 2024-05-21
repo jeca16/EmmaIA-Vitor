c6a720258b6e8f7c9ad8e8a752303039e5b931f2
JESSICA PEREIRA DOS SANTOS <23111734@CFP127ALUNO.LOCAL>
2024-05-21 15:57:54 -0300

    adicionar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('send--request')
 
 
-const consultaGemini = asyn (question => {
+const consultaGemini = (question => {
     const keyGoogle = 'AIzaSyBZaybh57iVi23jcLvzuIrabNG4f3td60A'
 
     const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + keyGoogle;
@@ -29,18 +29,18 @@ const consultaGemini = asyn (question => {
     fetch (url, requestOptions)
     .then(response => response.json())
     .then(data =>{
-        const respostaTextIa = data.candidates[0].content.parts[0].text
-        respostaTextIa(respostaTextIa)
+        const responseTextIa = data.candidates[0].content.parts[0].text
+        respostaIa(responseTextIa)
     })
 })
 
-const respostaIa = (respostaTextIa) =>{
+const respostaIa = (responseTextIa) =>{
     const textAreaPt = document.getElementById('answer-pt')
-    text
+    textAreaPt.value = responseTextIa
 }
 
 
 button.addEventListener('click', () =>{
-    constquestion = document.getElementById('ask--user').value
+    const question = document.getElementById('ask--user').value
     consultaGemini(question)
 })
